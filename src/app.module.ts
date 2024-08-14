53fe43e90eb5a0ef22aefc00f9e18d9951d819c4
sutthipong.nua <sutthiphongnuanma@gmail.com>
2024-08-14 10:45:43 +0700

    middleware and Exception

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  // RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { SongController } from './song/song.controller';
 import { SongModule } from './song/song.module';
 
 @Module({
@@ -8,4 +15,13 @@ import { SongModule } from './song/song.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggerMiddleware).forRoutes('songs'); // option no 1
+    // consumer
+    //   .apply(LoggerMiddleware)
+    //   .forRoutes({ path: 'songs', method: RequestMethod.POST }); //option no 2
+
+    consumer.apply(LoggerMiddleware).forRoutes(SongController); //option no 3
+  }
+}
