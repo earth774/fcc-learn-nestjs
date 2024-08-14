df26b385e7600bc1824c7e318b22bec3363e99c2
sutthipong.nua <sutthiphongnuanma@gmail.com>
2024-08-14 10:16:00 +0700

    validate api field

--- src/song/song.controller.ts
+++ src/song/song.controller.ts
@@ -1,20 +1,48 @@
-import { Controller, Get, Put, Delete, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Put,
+  Delete,
+  Post,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Body,
+} from '@nestjs/common';
 import { SongService } from './song.service';
+import { CreateSongDTO } from './dto/create-song-dto';
 
 @Controller('songs')
 export class SongController {
-  constructor(private songsService: SongService) {}
+  constructor(private songService: SongService) {}
   @Post()
-  create() {
-    return this.songsService.create('Animals by Martin Garrix');
+  create(@Body() createSongDTO: CreateSongDTO) {
+    return this.songService.create(createSongDTO);
   }
   @Get()
   findAll() {
-    return this.songsService.findAll();
+    try {
+      return this.songService.findAll();
+    } catch (e) {
+      throw new HttpException(
+        'server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
+    }
   }
   @Get(':id')
-  findOne() {
-    return 'fetch song on the based on id';
+  findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
+    return `fetch song on the based on id ${typeof id}`;
   }
 
   @Put(':id')
